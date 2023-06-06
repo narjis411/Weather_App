5e1730b08a5e9caaa8c871ff843293f4fc4fcabf
Narjis Aljebur <narjisaljebur@gmail.com>
2023-06-06 22:14:34 +1000

    option loading

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "f1fced8230msh767d50ac2a80736p1caa00jsn97abe5754050",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+//   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+//   export const WEATHER_API_KEY = ""; // enter your key from openweather API
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import {GEO_API_URL, geoApiOptions} from '../../api'
 
-const Search = (onSearchChange) => {
+const Search = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
+
+
+    const loadOptions = (inputValue) => {
+        return  fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions)
+
+            // .then((response) => response.json())
+            // .then((response) => {
+            // }
+        }
+
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
-    }
-    return(
+    };
+    
+    return (
         <AsyncPaginate
             placeholder="Search for city"
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
